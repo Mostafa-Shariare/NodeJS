c27cbf22d8fb4331d23a4ac3561f370d0f613402
Mostafa Shariare <158030087+Mostafa-Shariare@users.noreply.github.com>
2025-02-21 02:02:09 +0600

    Add files via upload

---
+++ Modules/index.js
@@ -0,0 +1,8 @@
+//to run this file
+//node index.js
+//use this command in terminal
+ 
+//importing modules
+const students = require("./srudents");
+console.log(`my name is ${students.getName()}`);
+console.log(`my age is ${students.getAge()}`);
